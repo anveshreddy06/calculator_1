4a1d6298f79f08ecc0d5b5d18c16681af816beb1
ash <anveshreddya06@gmail.com>
2026-04-08 15:07:15 +0530

    new update

--- calculator.py
+++ calculator.py
@@ -8,6 +8,8 @@ if 'operation' not in st.session_state:
     st.session_state.operation = ""
 if 'n' not in st.session_state:
     st.session_state.n = ""
+if 'result' not in st.session_state:
+    st.session_state.result = ""
 
 rows = 3
 cols = 3
@@ -51,6 +53,9 @@ with clear:
         st.session_state.num1 = ""
         st.session_state.operation = ""
         st.session_state.n = ""
+        st.session_state.result = ""
+
+
 with cal:
     if st.button('calculate'):
         if st.session_state.operation == "":
@@ -65,14 +70,17 @@ with cal:
                 st.write("Error: Invalid number input.")
             else:
                 if st.session_state.operation == '+':
-                    st.write('sum:', n1 + n2)
+                    #st.write(st.session_state.n,st.session_state.operation,st.session_state.num1)
+                    st.session_state.result = n1 + n2
                 elif st.session_state.operation == '-':
-                    st.write('difference:', n1 - n2)
+                    st.session_state.result = n1 - n2
                 elif st.session_state.operation == '*':
-                    st.write('product:', n1 * n2)
+                    st.session_state.result = n1 * n2
                 elif st.session_state.operation == '/':
                     if n2 == 0:
                         st.write("Error: Division by zero.")
                     else:
-                        st.write('quotient:', n1 / n2)
+                        st.session_state.result = n1 / n2
+                        
 
+st.write( st.session_state.n,st.session_state.operation,st.session_state.num1,'=', st.session_state.result)
